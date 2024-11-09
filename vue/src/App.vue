1f4621f289a2fcd7242c7d2555d614ac896fba52
Vladimir <worobev2001@gmail.com>
2024-11-09 11:06:21 +0300

    fifteen puzzle game

---
+++ vue/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <FifteenPuzzle />
+  </div>
+</template>
+
+<script>
+import FifteenPuzzle from "./components/FifteenPuzzle.vue";
+
+export default {
+  name: "App",
+  components: {
+    FifteenPuzzle,
+  },
+};
+</script>
